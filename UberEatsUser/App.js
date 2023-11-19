df76621f5fb5fda33efb25200ec3738a371f3866
Agon24Misini <agonmisini26@gmail.com>
2023-11-19 14:08:02 +0100

    Menu Item Detailed Screen

--- UberEatsUser/App.js
+++ UberEatsUser/App.js
@@ -3,12 +3,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, FlatList } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
 import RestaurantDetailsScreen from "./src/screens/RestaurantDetailsScreen";
+import DishDetailsScreen from "./src/screens/DishDetailsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <HomeScreen /> */}
-      <RestaurantDetailsScreen />
+      {/* <RestaurantDetailsScreen /> */}
+      <DishDetailsScreen />
       <StatusBar style='light' />
     </View>
   );
---
+++ UberEatsUser/src/screens/DishDetailsScreen/index.js
@@ -0,0 +1,52 @@
+import { Text, View } from "react-native";
+import restaurants from "../../../assets/data/restaurants.json";
+import styles from "./styles";
+import { AntDesign } from "@expo/vector-icons";
+import { useState } from "react";
+
+const dish = restaurants[0].dishes[0];
+
+const DishDetailsScreen = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  const onMinus = () => {
+    if (quantity > 1) setQuantity(quantity - 1);
+  };
+  const onPlus = () => {
+    setQuantity(quantity + 1);
+  };
+  const getTotal = () => {
+    return (dish.price * quantity).toFixed(2);
+  };
+
+  return (
+    <View style={styles.page}>
+      <Text style={styles.name}>{dish.name}</Text>
+      <Text style={styles.description}>{dish.description}</Text>
+      <View style={styles.seperator} />
+
+      <View style={styles.row}>
+        <AntDesign
+          name='minuscircleo'
+          size={60}
+          color='black'
+          onPress={onMinus}
+        />
+        <Text style={styles.quantity}>{quantity}</Text>
+        <AntDesign
+          name='pluscircleo'
+          size={60}
+          color='black'
+          onPress={onPlus}
+        />
+      </View>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>
+          Add {quantity} to basket (${getTotal()})
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default DishDetailsScreen;
